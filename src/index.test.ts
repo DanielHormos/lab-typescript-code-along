a98e263fd6872c38f9b5cc5ce97fe588b96f078a
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-08 15:36:20 +0200

    added test to uppercaseName for empty arrays

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,18 @@
 import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 
-test("It works!", () => {
-  deepEqual(0, 1);
+//const data = {{id: "f370acf7-76e6-42de-81f5-233cf971fa14", name: "Daniel Hormos"}, {id: "4c62bc06-253d-4fbb-8595-440f060cd1b0", name: "Ali Reza"}
+
+type Person = {
+  id: string;
+  name: string;
+};
+
+function uppercaseName(names: Person[]) {
+  return [];
+}
+
+test("UpperCaseName workd with empty arrays", () => {
+  const result = uppercaseName([]);
+  deepEqual(result, []);
 });
