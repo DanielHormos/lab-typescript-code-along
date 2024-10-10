8b308ef0e0a5198e26b05c9852e8374bdf5d56f9
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-10 11:17:25 +0200

    test for checking amount of element tag and also checking if element tag has class name

--- src/jsdom.test.ts
+++ src/jsdom.test.ts
@@ -1,19 +1,30 @@
-import { deepEqual } from "assert";
-import { equal } from "assert/strict";
+import { equal, deepEqual } from "assert/strict";
 import { JSDOM } from "jsdom";
 import test from "node:test";
 
 test("select paragraph and acces textcontent", () => {
-  const dom = new JSDOM(`<!DOCTYPE html><p>Hi</p>`);
+  const dom = new JSDOM(`<!DOCTYPE html><p>Hi!</p>`);
 
   equal(dom.window.document.querySelector("p")!.textContent, "Hi!");
 });
 
-test.skip("", () => {
+test("Check how many p tags in file", () => {
   const dom = new JSDOM(`
     <!DOCTYPE html>
-    p>Hi</p>
+    <p>Hi</p>
+    <p>Hi</p>
+    <p>Hi</p>
     <p>HidyHow!</p>`);
   const result = dom.window.document.querySelectorAll("p").length;
-  deepEqual(result, 2);
+  deepEqual(result, 4);
+});
+
+test("check if element has a certain class", () => {
+  const dom = new JSDOM(`<!DOCTYPE html>
+    <p class="nice">Hi</p>`);
+
+  const anchor = dom.window.document.querySelector("p");
+  const hasNiceClass = anchor.classList.contains("nice");
+  deepEqual(hasNiceClass, true);
+  console.log(hasNiceClass);
 });
